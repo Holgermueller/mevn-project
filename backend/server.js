a7d1ad014204ad3f5347611d399affa820650ba6
Holger Mueller <9jholger@gmail.com>
2022-05-26 21:20:57 -0500

    got server running

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => {
+  console.log("Api server listening on PORT ".underline.green + PORT);
+});
