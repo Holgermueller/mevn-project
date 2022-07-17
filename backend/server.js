112b23f22dca7b9d4a0dd51bc2c84b37c29c954c
Holger Mueller <9jholger@gmail.com>
2022-07-17 10:09:35 -0400

    connected to db

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 8080;
+
+connectDB();
 
 const app = express();
 
